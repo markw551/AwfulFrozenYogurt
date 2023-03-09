8c0fbcaf2ed7f6ecb6bb654ecea13946f2933adf
KAPsLuck <78551770+KAPsLuck@users.noreply.github.com>
2023-03-09 13:07:42 -0500

    Movement now in Testing with 2 Players

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -11,14 +11,16 @@ public class PlayerMovement : MonoBehaviour
 
     public class Movement
     {
-        public GameObject player;
+        public string Player;
         public string UpKey;
         public string DownKey;
         public string LeftKey;
         public string RightKey;
 
-        public Movement(string upKey, string downKey, string leftKey, string rightKey)
+        public Movement(string player, string upKey, string downKey, string leftKey, string rightKey)
         {
+            this.Player = player;
+
             this.UpKey = upKey;
             Input.GetKey(upKey);
 
@@ -36,26 +38,29 @@ public class PlayerMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Movement Player1 = new Movement("w", "s", "a", "d");
-        Movement Player2 = new Movement("up", "down", "left", "right");
+        Movement Player1 = new Movement("Player 1", "w", "s", "a", "d");
+        Movement Player2 = new Movement("Player 2", "up", "down", "left", "right");
     }
 
     // Update is called once per frame
     void Update()
-    {
-        if (Input.GetKey("w") || Input.GetKey("up"))
+    { 
+        if (gameObject.tag == "Player 1" && Input.GetKey("w") || gameObject.tag == "Player 2" && Input.GetKey("up"))
         {
             transform.Translate(Vector2.up * speed * Time.deltaTime);
         }
-        if (Input.GetKey("s") || Input.GetKey("down"))
+
+        if (gameObject.tag == "Player 1" && Input.GetKey("s") || gameObject.tag == "Player 2" && Input.GetKey("down"))
         {
             transform.Translate(Vector2.down * speed * Time.deltaTime);
         }
-        if (Input.GetKey("a") || Input.GetKey("left"))
+
+        if (gameObject.tag == "Player 1" && Input.GetKey("a") || gameObject.tag == "Player 2" && Input.GetKey("left"))
         {
             transform.Translate(Vector2.left * speed * Time.deltaTime);
         }
-        if (Input.GetKey("d") || Input.GetKey("right"))
+
+        if (gameObject.tag == "Player 1" && Input.GetKey("d") || gameObject.tag == "Player 2" && Input.GetKey("right"))
         {
             transform.Translate(Vector2.right * speed * Time.deltaTime);
         }
