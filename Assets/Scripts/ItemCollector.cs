62bfa9a8ad2db010f0f006e69d9c17ffb51ec2ca
Mark Wu <88671288+markw551@users.noreply.github.com>
2023-03-20 16:32:54 -0400

    reworked item collection

--- Assets/Scripts/ItemCollector.cs
+++ Assets/Scripts/ItemCollector.cs
@@ -4,11 +4,39 @@ using UnityEngine;
 
 public class ItemCollector : MonoBehaviour
 {
-    private void OnTriggerEnter2D(Collider2D collision)
+    public int score = 0;
+
+    public class ScoreTracker
+    {
+        public string Player; 
+        public int Score;
+
+        public ScoreTracker(string player, int score)
+        {
+            this.Score = score;
+            this.Player = player;
+
+        }
+    }
+
+    public void Start()
+    {
+        ScoreTracker Player1 = new ScoreTracker("Player 1", 0);
+        ScoreTracker Player2 = new ScoreTracker("Player 2", 0);
+    }
+
+    public void Update()
+    {
+
+}
+
+public void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Collectibles"))
         {
             Destroy(collision.gameObject);
+            score++;
+            Debug.Log(score);
         }
     }
 }
