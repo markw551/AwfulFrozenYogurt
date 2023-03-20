3d039ad41b07301a8ace8b5c3dce9686f6c041b1
David Bi <david.bi23@ycdsbk12.ca>
2023-03-20 14:24:55 -0400

    reworked doc movement method

--- Assets/Scripts/DoctorMovement.cs
+++ Assets/Scripts/DoctorMovement.cs
@@ -2,78 +2,47 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DoctorMovement : MonoBehaviour
-{
-    public class Doctor : MonoBehaviour
-    {
-        // finds which player is closest
-        public DocSearch(PlayerOneX, PlayerOneY, PlayerTwoX, PlayerTwoY)
-        {
-            this.PlayerOneDistance = math.sqrt(PlayerOneX * PlayerOneX + PlayerOneX * PlayerOneX)
-            this.PlayerTwoDistance = math.sqrt(PlayerTwoX * PlayerTwoX + PlayerTwoY * PlayerTwoY)
-
-            // closer to player 1 using the pythagorean theorem
-            if (this.PlayerOneDistance > PlayerTwoDistance)
-            {
-                this.ClosestPlayer = "Player 1"
-            }
-            //closer to player 2
-            else
-            {
-                this.ClosestPlayer = "Player 2"
-            }
-        }
+public class DoctorMovement : MonoBehaviour{
+    public class Doctor : MonoBehaviour{
+        
 
         // doctor moving towards the player
-        public DocMove(PlayerOneX, PlayerOneY, PlayerTwoX, PlayerTwoY)
-        {
-            if (Doctor.ClosestPlayer = "Player 1")
-            {
-                // move in y direction
-                if (math.abs(PlayerOneX) > math.abs(PlayerOneY) && PlayerOneY != 0)
-                {
-                    if(PlayerOneY < 0)
-                    {
-                        transform.Translate(Vector2.down * speed * Time.deltaTime)
-                    }
-                    else
-                    {
-                        transform.Translate(Vector2.up * speed * Time.deltatime)
-                    }
-                }
-                else
-                {
-                    if(PlayerOneX < 0)
-                    {
-                        transform.Translate(Vector2.left * speed * Time.deltaTime)
-                    }
-                    else
-                    {
-                        transform.Translate(Vector2.right * speed 8 Time.deltaTime)
-                    }
-                }
-            }
-            else
-            {
+        public DocMove(PlayerOneX, PlayerOneY, PlayerTwoX, PlayerTwoY){
+            
+            // finding closest direction
+            List<int> coordinates = new List<int> {PlayerOneX, PlayerOneY, PlayerTwoX, PlayerTwoY};
+            
+            // finding smallest non zero number
+            coordinates.Sort();
+            if (PlayerOneX = coordinates.Min
 
+            if (DocMove.direction = "Up"){
+                transform.Translate(Vector2.up * speed * Time.deltaTime)
+            }
+            if (DocMove.direction = "Down"){
+                transform.Translate(Vector2.down * speed * Time.deltaTime)
+            }
+            if (DocMove.direction = "Left"){
+                transform.Translate(Vector2.left * speed * Time.deltaTime)
+            }
+            if (DocMove.direction = "Right"){
+                transform.Translate(Vector2.right * speed * Time.deltaTime)
             }
         }
+    }
 
         //destroys anything blocking the doctors way
-        public BreakBlock()
-        {
+        public BreakBlock(){
 
         }
     }
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start(){
         
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update(){
         
     }
 }
